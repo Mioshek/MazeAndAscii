2ff59d7a1d322a1e9c306be985be75235f15f9cc
Mioshek <milosz.szczygiel@interia.pl>
2023-03-30 23:59:40 +0200

    shifted font fixed and also added jbrains mono

--- MazeAndAscii/src/main/kotlin/fonts/FontsLoader.kt
+++ MazeAndAscii/src/main/kotlin/fonts/FontsLoader.kt
@@ -5,14 +5,14 @@ import imgui.ImGuiIO
 
 class FontsLoader(private val minSize: Int, private val maxSize: Int) {
     private val gruppoFonts = mutableListOf<ImFont>()
+    private val jBrainsMonoFonts = mutableListOf<ImFont>()
+    private val jBrainsMonoVectorFont = this.javaClass.getResourceAsStream("/fonts/JetBrainsMono-Regular.ttf").readAllBytes()
     private val gruppoVectorFont = this.javaClass.getResourceAsStream("/fonts/Chakra_Petch/ChakraPetch-Regular.ttf").readAllBytes()
     private val imIO: ImGuiIO = ImGui.getIO()
     private val atlas: ImFontAtlas = imIO.fonts
 
     fun setDefaultFont(fontSize: Int) {
         atlas.locked = false
-//           val indieFlower = this.javaClass.getResourceAsStream("/fonts/Indie_Flower/Indie_Flower-Regular.ttf").readAllBytes()
-//           val chakraPetch = this.javaClass.getResourceAsStream("/fonts/Chakra_Petch-Regular.ttf").readAllBytes()
         imIO.fontAllowUserScaling = true
         imIO.setFontDefault(gruppoFonts[fontSize - minSize - 1])
     }
@@ -20,14 +20,18 @@ class FontsLoader(private val minSize: Int, private val maxSize: Int) {
     fun loadFonts(){
         for (i in minSize..maxSize){
             gruppoFonts.add(atlas.addFontFromMemoryTTF(gruppoVectorFont, i.toFloat()))
+            jBrainsMonoFonts.add(atlas.addFontFromMemoryTTF(jBrainsMonoVectorFont, i.toFloat()))
         }
     }
-    fun changeFontSize(fontSize: Int){
+    fun changeFontSize(fontSize: Int, fontFamily: String){
         try{
-            ImGui.pushFont(gruppoFonts[fontSize - minSize -1])
+            when(fontFamily){
+                "gruppo" -> ImGui.pushFont(gruppoFonts[fontSize - minSize -1])
+                "jbrains" -> ImGui.pushFont(jBrainsMonoFonts[fontSize - minSize -1])
+            }
         }
         catch (e:java.lang.Exception){
-            println("Can't set fontSize to $fontSize")
+            println("Can't set fontSize to $fontSize, $fontFamily")
         }
     }
     fun popFont(){
